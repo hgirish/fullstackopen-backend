b3feb926dbc8bd755b6fa29443048011ce15129b
Girish <girish@post.com>
2024-07-16 14:48:30 -0700

    part4-1

--- models/note.js
+++ models/note.js
@@ -1,30 +1,12 @@
-
-
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URL
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-.then(result => {
-    console.log('connected to MongoDB')
-})
-.catch(error=>{
-    console.error('error connecting to MongoDB:', error.message)
-})
-
-
-
 const noteSchema = new mongoose.Schema({
-    content:{
-      type: String,
-      minLength: 5,
-      required:true
-    },
-    important: Boolean,
+  content:{
+    type: String,
+    minLength: 5,
+    required:true
+  },
+  important: Boolean,
 })
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
